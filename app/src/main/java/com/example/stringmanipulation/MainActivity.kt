7c0d0e48f5ba95d7374123d5af6c95e447c2e585
Zlatan Stadler <zlat0179@edu.easj.dk>
2022-02-05 12:14:56 +0100

    Added binding method Uppercase btn

--- app/src/main/java/com/example/stringmanipulation/MainActivity.kt
+++ app/src/main/java/com/example/stringmanipulation/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.stringmanipulation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stringmanipulation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.upperCaseButton.setOnClickListener {
+            val text = binding.inputEditText.text.trim().toString()
+            if (text.isEmpty()) {
+                binding.inputEditText.error = "No input"
+                return@setOnClickListener
+            }
+            val result = text.uppercase()
+            binding.resultTextView.text = result
+        }
     }
 }
